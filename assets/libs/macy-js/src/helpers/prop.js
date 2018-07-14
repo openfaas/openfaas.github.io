fc1197ad9a34f0540199926d01bcc1e51a1bd970
John McCabe <john@johnmccabe.net>
2018-07-14 20:22:13 +0100

    Delete old site
    Signed-off-by: John McCabe <john@johnmccabe.net>

--- assets/libs/macy-js/src/helpers/prop.js
+++
@@ -1,9 +0,0 @@
-/**
- * Get Element computed style
- * @param  {HTMLElement} element  - Element
- * @param  {String}      property - Property to fetch
- * @return {String}               - Desired Element Property
- */
-export default (element, property) => {
-  return window.getComputedStyle(element, null).getPropertyValue(property)
-}

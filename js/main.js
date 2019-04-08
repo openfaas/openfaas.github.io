66f1a6280211bce84d7aa900c32ea55ba94c2618
Alin Iacob <aliniacb@gmail.com>
2019-04-08 16:06:14 +0100

    add users and dynamic nav size
    Signed-off-by: Alin Iacob <aliniacb@gmail.com>

--- js/main.js
+++ js/main.js
@@ -75,8 +75,43 @@ function getGithubStars() {
     }
 }
 
+/*-------------Shrinks the top navbar---------------*/
+/*--------------------------------------------------*/
+
+function shrinkNav() {
+    var nav = document.getElementById('top-nav');
+    var htmlTag = document.documentElement;
+
+    var maxHeight = 184;
+    var minHeight = 80;
+
+    var scrolled = window.scrollY;
+
+    if (scrolled === 0) {
+        nav.style.height = maxHeight + 'px';
+        htmlTag.style.paddingTop = maxHeight + 'px';
+        return;
+    }
+
+    if (scrolled >= maxHeight) {
+        nav.style.height = minHeight + 'px';
+        htmlTag.style.paddingTop = minHeight + 'px';
+        return;
+    }
+
+    var height = Math.max(maxHeight - scrolled, minHeight) + 'px';
+
+    nav.style.height = height;
+    htmlTag.style.paddingTop = height;
+}
+
+window.addEventListener('scroll', function() {
+    shrinkNav();
+});
+
 /*------------Init scripts on pageload--------------*/
 /*--------------------------------------------------*/
 document.addEventListener('DOMContentLoaded', function() {
     getGithubStars();
+    shrinkNav();
 })

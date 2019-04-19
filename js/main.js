cba312bbe16046052cae05b9aa956a9a4509504c
Alin Iacob <aliniacb@gmail.com>
2019-04-19 10:49:30 +0100

    play video on page, change sponsors
    Signed-off-by: Alin Iacob <aliniacb@gmail.com>

--- js/main.js
+++ js/main.js
@@ -53,9 +53,100 @@ function initNavToggler() {
     });
 }
 
+/*------------Init youtube player modal-------------*/
+/*--------------------------------------------------*/
+
+function initVideoModal() {
+    var videoId = 'yOpYYYRuDQ0';
+    var triggers = document.querySelectorAll('.start-demo-video');
+
+    var modal = document.createElement('div');
+    modal.className = 'modal modal-youtube';
+
+    var background = document.createElement('div');
+    background.className = 'modal-background';
+
+    var content = document.createElement('div');
+    content.className = 'modal-content';
+
+    var player = document.createElement('div');
+    player.id = 'modal-player';
+
+    var close = document.createElement('button');
+    close.className = 'modal-close is-large';
+
+    modal.appendChild(background);
+    content.appendChild(player);
+    modal.appendChild(content);
+    modal.appendChild(close);
+
+    document.body.appendChild(modal);
+
+    function showModal(visible) {
+        if (visible) {
+            modal.classList.add('is-active');
+            modalPlayer.playVideo();
+        } else {
+            modal.classList.remove('is-active');
+            modalPlayer.stopVideo();
+        }
+    }
+
+    Array.from(triggers).forEach(function(el) {
+        el.addEventListener('click', function() {
+            showModal(true);
+        });
+    });
+
+    close.addEventListener('click', function() {
+        showModal(false);
+    });
+
+    background.addEventListener('click', function() {
+        showModal(false);
+    });
+}
+
+function onYouTubeIframeAPIReady() {
+    if (document.body.className === 'landing-page') {
+        landingPlayer = new YT.Player('yt-player-iframe', {
+            videoId: videoId,
+            playerVars: { 'autoplay': 0, 'controls': 1 },
+            events: {
+                'onReady': function(e) {
+                    e.target.a.classList.add('ready')
+                },
+                'onError': function(err) { console.log(err);}
+            }
+        });
+    }
+
+    modalPlayer = new YT.Player('modal-player', {
+        videoId: videoId,
+        playerVars: { 'autoplay': 0, 'controls': 1 },
+        events: {
+            'onReady': function(e) {
+                e.target.a.classList.add('ready')
+            },
+            'onError': function(err) { console.log(err);}
+        }
+    });
+}
+
+function initYoutubePlayerApi() {
+    var tag = document.createElement('script');
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+
 /*------------Init scripts on pageload--------------*/
 /*--------------------------------------------------*/
 document.addEventListener('DOMContentLoaded', function() {
     shrinkNav();
     initNavToggler();
+    initYoutubePlayerApi();
+    initVideoModal();
 })
--- js/youtubePlayer.js
+++ js/youtubePlayer.js
@@ -1,41 +1,23 @@
 /*--------------Initialize youtube api--------------*/
 /*--------------------------------------------------*/
 
-var player = null;
 var videoId = 'yOpYYYRuDQ0';
 var playerControl = document.getElementById('yt-player-play')
 var play = null;
 var stop = null;
+
+// landingPlayer is initialized in main.js and set as global variable
 
 function onPlayerReady(event) {
     event.target.playVideo();
 }
 
 function playVideo() {
-    player.playVideo();
-    player.a.classList.add('playing');
-}
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('yt-player-iframe', {
-        videoId: videoId,
-        playerVars: { 'autoplay': 0, 'controls': 1 },
-        events: {
-            'onReady': function(e) {
-                e.target.a.classList.add('ready')
-            },
-            'onError': function(err) { console.log(err);}
-        }
-    });
+    landingPlayer.playVideo();
+    landingPlayer.a.classList.add('playing');
 }
 
 function initYoutubePlayer() {
-    var tag = document.createElement('script');
-    var firstScriptTag = document.getElementsByTagName('script')[0];
-
-    tag.src = "https://www.youtube.com/iframe_api";
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
     playerControl.addEventListener('click', function() {
         playVideo();
     })

2164cfadb49917ec07d70d81ce0559dcf52fd436
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-04-24 21:19:19 +0100

    Build rows from shuffled contributors
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- generate-stats.js
+++ generate-stats.js
@@ -86,11 +86,11 @@ function generateTemplate(data) {
 			});
 		}
 
-		function shuffle(array) {
-			for (let i = array.length - 1; i > 0; i--) {
+		function shuffle(arr) {
+			for (let i = arr.length - 1; i > 0; i--) {
 				let j = Math.floor(Math.random() * (i + 1));
 
-				[array[i], array[j]] = [array[j], array[i]];
+				[arr[i], arr[j]] = [arr[j], arr[i]];
 			}
 		}
 
@@ -110,7 +110,7 @@ function generateTemplate(data) {
     			rowsString += '<div class="user-row">';
 
     			for (var i = 0; i < r; i++) {
-    				rowsString += userTemplate(contributors.shift());
+    				rowsString += userTemplate(filtered.shift());
     			}
 
     			rowsString += '</div>';

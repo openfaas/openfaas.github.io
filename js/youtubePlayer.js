c7f275963d0ff854fc0fdb6ae0b82a96a089e7e3
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-12-14 11:13:05 +0000

    Remove YouTube iframe and fix blog background
    * Removes faulty YouTube iframe for embedded playback
    * Changes to PLONK stack video rather than faasd video
    * Fixes backgrond (back to white) on blog article page
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- js/youtubePlayer.js
+++ js/youtubePlayer.js
@@ -9,24 +9,11 @@ var stop = null;
 
 // landingPlayer is initialized in main.js and set as global variable
 
-function onPlayerReady(event) {
-    console.log('Video player ready')
-    event.target.playVideo();
-}
-
-function playVideo() {
-    if(document.landingPlayer) {
-        document.landingPlayer.playVideo();
-        document.landingPlayer.f.classList.add('playing');
-        sampleVideo.classList.add('is-hidden');
-        playerControl.classList.add('is-hidden');
-    }
-}
-
 function initYoutubePlayer() {
-    playerControl.addEventListener('click', function() {
-        playVideo();
-    })
+    // playerControl.addEventListener('click', function() {
+
+    //     console.log("Got here")
+    // })
 }
 
 /*------------Init scripts on pageload--------------*/
